6ab16c460a7f0527b1c6b2d187f0dca1f02e61b4
Matheus de Sá <85846478+matheus0sa@users.noreply.github.com>
2024-03-30 23:30:36 -0300

    doc: Documentação

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from setuptools import setup
 
+f = open('README.md')
+
+long = f.read()
+
+f.close()
+
+
 setup(
     name="apyoripandas",
     version="0.1.0",
@@ -9,4 +16,5 @@ setup(
     url="https://github.com/matheus0sa/apyoripandas",
     packages=["apyoripandas"],
     python_requires=">=3.6",
+    long_description=long
 )
